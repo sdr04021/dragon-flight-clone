428ed5b148537dcd1c318cd67538bebe7c40572d
cognivolu <83330604+cognivolu@users.noreply.github.com>
2021-10-25 21:08:35 +0900

    Update Test.cs
    test

--- DragonFlightClone/Assets/Scripts/Test.cs
+++ DragonFlightClone/Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Å×½ºÆ® ÀÌ½Â¿±
     }
 
     // Update is called once per frame
