1e3e9ee1c93d16ef6e3823617d53d1673605fde6
sdr04021 <43779092+sdr04021@users.noreply.github.com>
2021-10-30 14:42:37 +0900

    메인화면 수정

--- DragonFlightClone/Assets/Scripts/MainImage.cs
+++ DragonFlightClone/Assets/Scripts/MainImage.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainImage : MonoBehaviour
 {
+    Image touchToStart;
+    bool oneToZero = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        touchToStart = GameObject.Find("TouchToStart").GetComponent<Image>();
+        StartCoroutine(BlinkText()); 
     }
 
     // Update is called once per frame
@@ -17,6 +22,36 @@ public class MainImage : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene("LobbyScene");
+        }
+    }
+
+
+    public IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            if (oneToZero)
+            {
+                touchToStart.color = new Color(touchToStart.color.r, touchToStart.color.g, touchToStart.color.b, touchToStart.color.a - (Time.deltaTime));
+                if (touchToStart.color.a <= 0)
+                {
+                    yield return new WaitForSeconds(0.5f);
+                    touchToStart.color = new Color(touchToStart.color.r, touchToStart.color.g, touchToStart.color.b, 0);
+                    oneToZero = false;
+                }
+            }
+            else
+            {
+                touchToStart.color = new Color(touchToStart.color.r, touchToStart.color.g, touchToStart.color.b, touchToStart.color.a + (Time.deltaTime));
+                if (touchToStart.color.a >= 1)
+                {
+                    yield return new WaitForSeconds(0.5f);
+                    touchToStart.color = new Color(touchToStart.color.r, touchToStart.color.g, touchToStart.color.b, 1);
+                    oneToZero = true;
+                }
+            }
+
+            yield return null;
         }
     }
 }
